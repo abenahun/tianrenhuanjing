c00c0bc415d3dc4a4e80e61499b1a085bcae1ef4
XiaoQiu000 <18005428163@163.com>
2018-05-14 16:23:21 +0800

    产气排名

--- app/src/main/java/com/tianren/methane/fragment/YanYangFragment4.java
+++ app/src/main/java/com/tianren/methane/fragment/YanYangFragment4.java
@@ -1,55 +1,34 @@
 package com.tianren.methane.fragment;
 
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.tianren.methane.R;
-import com.tianren.methane.activity.DataStatisticsActivity;
-import com.tianren.methane.adapter.ModelAdapter;
-import com.tianren.methane.event.ModelEvent;
-import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import com.tianren.methane.utils.MPChartHelper;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.tianren.methane.activity.MainActivity.modelMap;
-import static com.tianren.methane.activity.MainActivity.sensorDataMap;
 
 /**
  * A simple {@link Fragment} subclass.
  *
  * @author qiushengtao
  */
-public class YanYangFragment4 extends BaseFragment implements View.OnClickListener {
-
-    private TextView tv_qiguishaixuan;
-    private Button btn_data;
-    private String[] items = {"厌氧罐 No1", "厌氧罐 No2", "厌氧罐 No3"};
-
-    private SwipeMenuRecyclerView recyclerView;
-    private ModelAdapter adapter;
+public class YanYangFragment4 extends BaseFragment {
     private View view;
+    private LineChart lineChart;
+    private List<String> xAxisValues;
+    private List<Float> yAxisValues;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EventBus.getDefault().register(this);
     }
 
     @Override
@@ -57,103 +36,22 @@ public class YanYangFragment4 extends BaseFragment implements View.OnClickListen
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_yan_yang_fragment4, container, false);
         initView();
-        loadData();
+        initData();
         return view;
     }
 
     private void initView() {
-        recyclerView = (SwipeMenuRecyclerView) view.findViewById(R.id.recyclerView);
-        adapter = new ModelAdapter(getActivity(), listener);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setItemViewSwipeEnabled(false);
-        View headView = LayoutInflater.from(getActivity()).inflate(R.layout.head_yanyang, recyclerView, false);
-        tv_qiguishaixuan = (TextView) headView.findViewById(R.id.tv_qiguishaixuan);
-        btn_data = (Button) headView.findViewById(R.id.btn_data);
-        tv_qiguishaixuan.setOnClickListener(this);
-        btn_data.setOnClickListener(this);
-        recyclerView.addHeaderView(headView);
-        recyclerView.setAdapter(adapter);
+        lineChart = (LineChart) view.findViewById(R.id.lineChart);
     }
 
-    private void loadData() {
-        List<ModelAdapter.ModelBean> list = new ArrayList<>();
-        if (sensorDataMap == null) {
-            list = null;
-        } else {
-            list.add(getModel("d37"));
-            list.add(getModel("d38"));
-            list.add(getModel("d39"));
-            list.add(getModel("d40"));
-            list.add(getModel("d41"));
-            list.add(getModel("d42"));
-            list.add(getModel("d43"));
-            list.add(getModel("d44"));
-            list.add(getModel("d45"));
-            list.add(getModel("d46"));
-            list.add(getModel("d47"));
-            list.add(getModel("d48"));
-            list.add(getModel("d49"));
-            list.add(getModel("d50"));
-            for (int i = 0; i < list.size(); i++) {
-                if (!TextUtils.isEmpty(list.get(i).getNickName().trim()) && !TextUtils.isEmpty(list.get(i).getData())) {
-                    adapter.addItem(list.get(i));
-                }
-            }
+    private void initData() {
+        xAxisValues = new ArrayList<>();
+        yAxisValues = new ArrayList<>();
+        for (int i = 1; i < 13; ++i) {
+            xAxisValues.add(String.valueOf(i));
+            yAxisValues.add((float) (Math.random() * 10 + 1000));
         }
+        MPChartHelper.setLineChart(lineChart, xAxisValues, yAxisValues, "聚类分析", true);
     }
 
-    public ModelAdapter.ModelBean getModel(String s) {
-        if (sensorDataMap == null) {
-            return null;
-        } else {
-            return new ModelAdapter.ModelBean(s, sensorDataMap.get(s).getNickName(), modelMap.get(s),
-                    sensorDataMap.get(s).getSuitableMaximum(), sensorDataMap.get(s).getSuitableMinimum(),
-                    sensorDataMap.get(s).getSensorUnit());
-        }
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_qiguishaixuan:
-                AlertDialog dialog = new AlertDialog.Builder(getActivity()).setTitle("厌氧罐筛选")
-                        .setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                tv_qiguishaixuan.setText(items[which]);
-                                dialog.dismiss();
-                            }
-                        }).create();
-                dialog.show();
-                break;
-
-            case R.id.btn_data:
-                break;
-            default:
-                break;
-        }
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onMessageEvent(ModelEvent event) {
-        loadData();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        EventBus.getDefault().unregister(this);
-    }
-
-    private ModelAdapter.ModelListener listener = new ModelAdapter.ModelListener() {
-
-        @Override
-        public void onClick(ModelAdapter.ModelBean bean) {
-            Intent intent7 = new Intent(getActivity(), DataStatisticsActivity.class);
-            intent7.putExtra("title", "厌氧走势");
-            intent7.putExtra("statisticsName", "厌氧走势");
-            startActivity(intent7);
-        }
-    };
 }
--- app/src/main/java/com/tianren/methane/fragment/YanYangFragment6.java
+++ app/src/main/java/com/tianren/methane/fragment/YanYangFragment6.java
@@ -1,159 +1,31 @@
 package com.tianren.methane.fragment;
 
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.tianren.methane.R;
-import com.tianren.methane.activity.DataStatisticsActivity;
-import com.tianren.methane.adapter.ModelAdapter;
-import com.tianren.methane.event.ModelEvent;
-import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.tianren.methane.activity.MainActivity.modelMap;
-import static com.tianren.methane.activity.MainActivity.sensorDataMap;
 
 /**
  * A simple {@link Fragment} subclass.
  *
  * @author qiushengtao
  */
-public class YanYangFragment6 extends BaseFragment implements View.OnClickListener {
-
-    private TextView tv_qiguishaixuan;
-    private Button btn_data;
-    private String[] items = {"厌氧罐 No1", "厌氧罐 No2", "厌氧罐 No3"};
-
-    private SwipeMenuRecyclerView recyclerView;
-    private ModelAdapter adapter;
+public class YanYangFragment6 extends BaseFragment  {
     private View view;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EventBus.getDefault().register(this);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_yan_yang_fragment6, container, false);
         initView();
-        loadData();
         return view;
     }
 
     private void initView() {
-        recyclerView = (SwipeMenuRecyclerView) view.findViewById(R.id.recyclerView);
-        adapter = new ModelAdapter(getActivity(), listener);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setItemViewSwipeEnabled(false);
-        View headView = LayoutInflater.from(getActivity()).inflate(R.layout.head_yanyang, recyclerView, false);
-        tv_qiguishaixuan = (TextView) headView.findViewById(R.id.tv_qiguishaixuan);
-        btn_data = (Button) headView.findViewById(R.id.btn_data);
-        tv_qiguishaixuan.setOnClickListener(this);
-        btn_data.setOnClickListener(this);
-        recyclerView.addHeaderView(headView);
-        recyclerView.setAdapter(adapter);
     }
 
-    private void loadData() {
-        List<ModelAdapter.ModelBean> list = new ArrayList<>();
-        if (sensorDataMap == null) {
-            list = null;
-        } else {
-            list.add(getModel("d37"));
-            list.add(getModel("d38"));
-            list.add(getModel("d39"));
-            list.add(getModel("d40"));
-            list.add(getModel("d41"));
-            list.add(getModel("d42"));
-            list.add(getModel("d43"));
-            list.add(getModel("d44"));
-            list.add(getModel("d45"));
-            list.add(getModel("d46"));
-            list.add(getModel("d47"));
-            list.add(getModel("d48"));
-            list.add(getModel("d49"));
-            list.add(getModel("d50"));
-            for (int i = 0; i < list.size(); i++) {
-                if (!TextUtils.isEmpty(list.get(i).getNickName().trim()) && !TextUtils.isEmpty(list.get(i).getData())) {
-                    adapter.addItem(list.get(i));
-                }
-            }
-        }
-    }
-
-    public ModelAdapter.ModelBean getModel(String s) {
-        if (sensorDataMap == null) {
-            return null;
-        } else {
-            return new ModelAdapter.ModelBean(s, sensorDataMap.get(s).getNickName(), modelMap.get(s),
-                    sensorDataMap.get(s).getSuitableMaximum(), sensorDataMap.get(s).getSuitableMinimum(),
-                    sensorDataMap.get(s).getSensorUnit());
-        }
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_qiguishaixuan:
-                AlertDialog dialog = new AlertDialog.Builder(getActivity()).setTitle("厌氧罐筛选")
-                        .setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                tv_qiguishaixuan.setText(items[which]);
-                                dialog.dismiss();
-                            }
-                        }).create();
-                dialog.show();
-                break;
-
-            case R.id.btn_data:
-                break;
-            default:
-                break;
-        }
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onMessageEvent(ModelEvent event) {
-        loadData();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        EventBus.getDefault().unregister(this);
-    }
-
-    private ModelAdapter.ModelListener listener = new ModelAdapter.ModelListener() {
-
-        @Override
-        public void onClick(ModelAdapter.ModelBean bean) {
-            Intent intent7 = new Intent(getActivity(), DataStatisticsActivity.class);
-            intent7.putExtra("title", "厌氧走势");
-            intent7.putExtra("statisticsName", "厌氧走势");
-            startActivity(intent7);
-        }
-    };
 }
