716863a327141a0239b9d75d772d7f126458eb96
Administrator <abenahun@163.com>
2018-06-12 13:16:15 +0800

    首页报表

---
+++ acommon/src/main/java/com/tianren/acommon/bean/GetAnerobicListBean.java
@@ -0,0 +1,12 @@
+package com.tianren.acommon.bean;
+
+import com.tianren.acommon.BaseResponse;
+
+/**
+ * Created by Administrator on 2018/6/12.
+ */
+
+public class GetAnerobicListBean extends BaseResponse<AnaerobicTankBean> {
+
+
+}
